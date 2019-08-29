b9ae60912d0c7f6e74737a657a38190342f1e3d1
vadnaireka <budaikreativ@gmail.com>
2019-08-29 23:31:17 +0200

    saving rating with context api

--- src/components/CardsofSearchResult.js
+++ src/components/CardsofSearchResult.js
@@ -34,7 +34,7 @@ class Cards extends Component {
                 <Card.Title className="card-title">{this.props.data.eventEntity.eventName}</Card.Title>
                 <Card.Text className="card-text">{this.props.data.eventEntity.date} {this.props.data.eventEntity.time}</Card.Text>
                 <Button className="btn" variant="primary" onClick={() => this.saveToDB(this.props.data.eventEntity)}>{this.state.button}</Button>
-                <Rating onClick={(rate) => this.saveRating(this.props.data, rate)} placeholderRating={this.props.data.averagerating}/>
+                <Rating onClick={(rate) => this.saveRating(this.props.data.eventEntity, rate)} placeholderRating={this.props.data.averagerating}/>
             </Card.Body>
         </Card>
     )
