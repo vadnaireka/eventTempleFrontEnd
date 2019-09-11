e9db53a7621607da98451c1355af5b281417bab7
Dávid Szabó <drszabodavid@gmail.com>
2019-09-11 13:56:14 +0200

    token support added

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,12 +5,12 @@ import axios from "axios";
 
 class Login extends Component {
     state = {
-        username : "",
-        password : ""
+        username: "",
+        password: ""
     };
 
-    onChange = (e) =>{
-        this.setState({[e.target.name] : e.target.value})
+    onChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
     };
 
     onSubmit = (e) => {
@@ -18,11 +18,12 @@ class Login extends Component {
         axios.post(`http://localhost:8080/validation/${this.props.auth}`, {
             username: this.state.username,
             password: this.state.password
-        }).then(
-            this.context.fetchData(`http://localhost:8080/validation/${this.props.auth}`, "userdata")
-        );
-
+        }).then(response => {
+            localStorage.set("token", response.data.token)
+            }
+        )
     };
+
 
     render() {
         return (
@@ -30,9 +31,11 @@ class Login extends Component {
             <div className="loginpage">
                 <h4 className="loginheader">{this.props.header}</h4>
                 <form className="login-form" onSubmit={this.onSubmit}>
-                    <input className="auth-input" type="text" name="username" onChange={this.onChange} value={this.state.username} placeholder="User Name"/><br/>
-                    <input className="auth-input" type="password" name="password" onChange={this.onChange} value={this.state.password} placeholder="Password"/><br/>
-                    <input type="submit"  value={this.props.auth}/>
+                    <input className="auth-input" type="text" name="username" onChange={this.onChange}
+                           value={this.state.username} placeholder="User Name"/><br/>
+                    <input className="auth-input" type="password" name="password" onChange={this.onChange}
+                           value={this.state.password} placeholder="Password"/><br/>
+                    <input type="submit" value={this.props.auth}/>
                 </form>
             </div>
             //</context.Consumer>
